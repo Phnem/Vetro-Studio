63b1c0f0c5b7a0fd22086a519a5f97ad83eb1b0b
Phnem <2004idelchik@gmail.com>
2026-06-12 17:42:58 +0300

    Fix: add basePath for GitHub Pages assets

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/Vetro',
-  assetPrefix: '/Vetro',
+  basePath: '/Vetro-Studio',
   images: {
     unoptimized: true,
   },
